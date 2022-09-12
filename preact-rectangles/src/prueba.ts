dc3a8427e0348d42457abe26c5fd71e70a8cebd8
agu17 <49200215+agu17@users.noreply.github.com>
2022-09-12 12:26:59 -0300

    Solucion conexion de interfaces
    Se soluciono el enlace entre interfaces

--- preact-rectangles/src/prueba.ts
+++ preact-rectangles/src/prueba.ts
@@ -1,9 +1,40 @@
-figma.showUI(__html__, {width: 320, height: 640, title:"Detector de Smells."});
+
+
+figma.showUI(__uiFiles__.main, {width: 320, height: 640, title:"Detector de Smells."});
 
 figma.ui.onmessage = msg => {
-    if(msg.type === 'salirPlugin'){
-        figma.closePlugin();
+
+    switch(msg.type){
+        case 'salirPlugin':
+            figma.closePlugin()
+            break;
+
+        case 'ingresarContenido':
+            figma.showUI(__uiFiles__.secondary, {width: 320, height: 640, title:"Detector de Smells."});
+            break;
+
+        case 'volver':
+            figma.showUI(__uiFiles__.main, {width: 320, height: 640, title:"Detector de Smells."});
+            break;
+        }
         
+
     }
 
-}
+    /*if(msg.type === 'salirPlugin'){
+        figma.closePlugin()
+    }
+    else {
+        if (msg.type === 'ingresarContenido') {
+        figma.showUI(__uiFiles__.secondary, {width: 320, height: 640, title:"Detector de Smells."});
+        }
+        else {
+            if (msg.type === 'volver') {
+            figma.showUI(__uiFiles__.main, {width: 320, height: 640, title:"Detector de Smells."});
+            }
+
+    }
+
+    }
+}*/
+
